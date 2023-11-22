8ae5244860b156e5bb9591f9f3cac5d70a2338b1
Elcharahma <151650389+Elcharahma@users.noreply.github.com>
2023-11-22 21:53:02 +0700

    Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,10 +5,14 @@ const Home = () => {
     <div className="jumb min-vh-100 w-100" id="home" >
         <Container>
             <Row>
+            <div data-aos="fade-up"data-aos-duration="2000">
+
                 <Col className="text-white text-center">
                    <h1>W E L C O M E</h1>
                    <p>Halo, mari kita mulai petualangan penjelajahan biodata ini bersama.</p>
+                   
                 </Col>
+                </div>
             </Row>
         </Container>
         </div>

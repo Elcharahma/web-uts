52ccd268cea8a70f7a7e1e1a01aefa77b6bbfeba
Elcharahma <151650389+Elcharahma@users.noreply.github.com>
2023-11-22 16:18:06 +0700

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div
+        className="futer">
+            <p>&copy; 2023 Elsha Rahma Listyanti</p>
+        
+    </div>
+  )
+}
+
+export default Footer;

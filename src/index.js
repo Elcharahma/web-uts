341be18c42c7930aba4ce28f82348fad1a36469e
Elcharahma <151650389+Elcharahma@users.noreply.github.com>
2023-11-22 22:19:21 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./css/main.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
